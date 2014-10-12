e187278746104ae72b9647197bdefa7206be210f
Oscar Moreno <Oscar Moreno@localhost>
2014-10-12 02:43:19 +0200

    refactor, extract files and classes

--- TakeMeThere/TaxiRegistrationTests.cs
+++ TakeMeThere/TaxiRegistrationTests.cs
@@ -23,25 +23,4 @@ namespace TakeMeThere
             availableTaxiRepository.Verify(x => x.Save(It.IsAny<AvailableTaxi>()));
         }
     }
-
-    [TestFixture]
-    public class TaxiBookingTests
-    {
-        [Test]
-        public void ShouldReturnBookingReferenceWhenNewBookingIsMade()
-        {
-            var availableTaxiRepository = new Mock<IAvailableTaxiRepository>();
-            var api = new Api(availableTaxiRepository.Object);
-            var taxi = new Taxi(TaxiSize.Small, 4, true, false, false, false);
-            var customer = new Customer();
-
-            var bookingReference = api.BookTaxi(taxi, customer, new Location(1, 1), new Location(1, 1), 150.3);
-
-            Assert.IsNotNull(bookingReference);
-        }
-    }
-
-    public class Customer
-    {
-    }
 }
