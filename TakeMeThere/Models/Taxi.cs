3b5ca82b1ba9b53cb4d8f1b36a77fa3248d87867
Oscar Moreno <Oscar Moreno@localhost>
2014-10-12 04:15:05 +0200

    green ShouldFilterMostAffordableTaxis

--- TakeMeThere/Models/Taxi.cs
+++ TakeMeThere/Models/Taxi.cs
@@ -11,6 +11,7 @@ namespace TakeMeThere.Models
         public bool WheelchairAccesible { get; private set; }
         public bool ExtraBaggageSpace { get; private set; }
         public bool LuxuriousEquipment { get; private set; }
+        public int Price { get; private set; }
 
         public Taxi(TaxiSize size, int numberOfSeats, bool airConditioned, bool wheelchairAccesible, bool extraBaggageSpace, bool luxuriousEquipment)
         {
@@ -21,6 +22,16 @@ namespace TakeMeThere.Models
             WheelchairAccesible = wheelchairAccesible;
             ExtraBaggageSpace = extraBaggageSpace;
             LuxuriousEquipment = luxuriousEquipment;
+            Price = CalculatePrice();
+        }
+
+        private int CalculatePrice()
+        {
+            return (int)Size + NumberOfSeats +
+                    (AirConditioned ? 1 : 0) +
+                    (WheelchairAccesible ? 1 : 0) +
+                    (ExtraBaggageSpace ? 1 : 0) +
+                    (LuxuriousEquipment ? 1 : 0);
         }
     }
 }
