a7ce73d43857f2b95e66d208956e4a87c0b1ccc7
Oscar Moreno <Oscar Moreno@localhost>
2014-10-12 02:34:30 +0200

    refactor, extract files and classes

---
+++ TakeMeThere/Models/Taxi.cs
@@ -0,0 +1,23 @@
+﻿namespace TakeMeThere.Models
+{
+    public class Taxi
+    {
+        public TaxiSize Size { get; private set; }
+        public int NumberOfSeats { get; private set; }
+        public bool AirConditioned { get; private set; }
+        public bool WheelchairAccesible { get; private set; }
+        public bool ExtraBaggageSpace { get; private set; }
+        public bool LuxuriousEquipment { get; private set; }
+
+        public Taxi(TaxiSize size, int numberOfSeats, bool airConditioned, bool wheelchairAccesible, bool extraBaggageSpace, bool luxuriousEquipment)
+        {
+            Size = size;
+            NumberOfSeats = numberOfSeats;
+            AirConditioned = airConditioned;
+            WheelchairAccesible = wheelchairAccesible;
+            ExtraBaggageSpace = extraBaggageSpace;
+            LuxuriousEquipment = luxuriousEquipment;
+        }
+
+    }
+}
--- TakeMeThere/Tests.cs
+++ TakeMeThere/Tests.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
+using TakeMeThere.Models;
+using TakeMeThere.Repositories;
 
 namespace TakeMeThere
 {
@@ -24,104 +22,5 @@ namespace TakeMeThere
 
             availableTaxiRepository.Verify(x => x.Save(It.IsAny<AvailableTaxi>()));
         }
-
-        public class TaxiAvailabilityPreferences
-        {
-            public TaxiTripLength TripLength { get; private set; }
-            public int MinimunCustomerRating { get; private set; }
-            public int WorkingLocationRadio { get; private set; }
-
-            public TaxiAvailabilityPreferences(TaxiTripLength tripLength, int minimunCustomerRating, int workingLocationRadio)
-            {
-                TripLength = tripLength;
-                MinimunCustomerRating = minimunCustomerRating;
-                WorkingLocationRadio = workingLocationRadio;
-            }
-        }
-
-        public class Taxi
-        {
-            public TaxiSize Size { get; private set; }
-            public int NumberOfSeats { get; private set; }
-            public bool AirConditioned { get; private set; }
-            public bool WheelchairAccesible { get; private set; }
-            public bool ExtraBaggageSpace { get; private set; }
-            public bool LuxuriousEquipment { get; private set; }
-
-            public Taxi(TaxiSize size, int numberOfSeats, bool airConditioned, bool wheelchairAccesible, bool extraBaggageSpace, bool luxuriousEquipment)
-            {
-                Size = size;
-                NumberOfSeats = numberOfSeats;
-                AirConditioned = airConditioned;
-                WheelchairAccesible = wheelchairAccesible;
-                ExtraBaggageSpace = extraBaggageSpace;
-                LuxuriousEquipment = luxuriousEquipment;
-            }
-
-        }
-
-        public enum TaxiSize
-        {
-            Small,
-            Medium,
-            Large
-        }
-
-        public enum TaxiTripLength
-        {
-            Short,
-            Long
-        }
-
-        public class Location
-        {
-            private double longitude;
-            private double latitude;
-
-            public Location(double latitude, double longitude)
-            {
-                this.latitude = latitude;
-                this.longitude = longitude;
-            }
-        }
-    }
-
-    public class AvailableTaxi
-    {
-        private readonly Tests.Taxi _taxi;
-        private readonly Tests.Location _currentLocation;
-        private readonly Tests.TaxiTripLength tripLength;
-        private readonly int minimunCustomerRating;
-        private readonly int workingLocationRadio;
-
-        public AvailableTaxi(Tests.Taxi taxi, Tests.Location currentLocation, Tests.TaxiAvailabilityPreferences taxiAvailabilityPreferences)
-        {
-            _taxi = taxi;
-            _currentLocation = currentLocation;
-            tripLength = taxiAvailabilityPreferences.TripLength;
-            minimunCustomerRating = taxiAvailabilityPreferences.MinimunCustomerRating;
-            workingLocationRadio = taxiAvailabilityPreferences.WorkingLocationRadio;
-        }
-    }
-
-    public class Api
-    {
-        private readonly IAvailableTaxiRepository availableTaxiRepository;
-
-        public Api(IAvailableTaxiRepository availableTaxiRepository)
-        {
-            this.availableTaxiRepository = availableTaxiRepository;
-        }
-
-        public void RegisterTaxi(Tests.Taxi taxi, Tests.Location currentLocation, Tests.TaxiAvailabilityPreferences taxiAvailabilityPreferences)
-        {
-            var availableTaxi = new AvailableTaxi(taxi, currentLocation, taxiAvailabilityPreferences);
-            availableTaxiRepository.Save(availableTaxi);
-        }
-    }
-
-    public interface IAvailableTaxiRepository
-    {
-        void Save(AvailableTaxi availableTaxi);
     }
 }
