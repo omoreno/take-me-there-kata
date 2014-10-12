463c49ed9504b8688665a50b0ffcb3485236ffe2
Oscar Moreno <Oscar Moreno@localhost>
2014-10-12 03:18:16 +0200

    refactor, extract files

---
+++ TakeMeThere/Models/BookingRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TakeMeThere.Models
+{
+    public class BookingRequest
+    {
+        public string Id { get; private set;  }
+        private readonly string taxiId;
+        private readonly string customerId;
+
+        public BookingRequest(string taxiId, string customerId)
+        {
+            Id = new Guid().ToString();
+            this.taxiId = taxiId;
+            this.customerId = customerId;
+        }
+    }
+}
