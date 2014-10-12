e70d9aa0d1d4cc6bbc3d5c7f089221d71d0dd909
Oscar Moreno <oscar.moreno.martin@gmail.com>
2014-10-12 03:17:41 +0200

    refactor, extract test setup

--- TakeMeThere/TaxiBookingTests.cs
+++ TakeMeThere/TaxiBookingTests.cs
@@ -10,13 +10,25 @@ namespace TakeMeThere
     [TestFixture]
     public class TaxiBookingTests
     {
+        private Api api;
+        private Mock<IBookingRepository> bookingRepository;
+        private Mock<IAvailableTaxiRepository> availableTaxiRepository;
+        private Taxi taxi;
+        private Customer customer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            availableTaxiRepository = new Mock<IAvailableTaxiRepository>();
+            bookingRepository = new Mock<IBookingRepository>();
+            api = new Api(availableTaxiRepository.Object, bookingRepository.Object);
+            taxi = new Taxi(TaxiSize.Small, 4, true, false, false, false);
+            customer = new Customer();
+        }
+
         [Test]
         public void ShouldReturnBookingReferenceWhenNewBookingIsMade()
         {
-            var availableTaxiRepository = new Mock<IAvailableTaxiRepository>();
-            var api = new Api(availableTaxiRepository.Object, null);
-            var taxi = new Taxi(TaxiSize.Small, 4, true, false, false, false);
-            var customer = new Customer();
             availableTaxiRepository
                     .Setup(x => x.Exists(It.IsAny<string>()))
                     .Returns(true);
@@ -29,10 +41,6 @@ namespace TakeMeThere
         [Test]
         public void ShouldThrowIfTaxiIsAlreadyBooked()
         {
-            var availableTaxiRepository = new Mock<IAvailableTaxiRepository>();
-            var api = new Api(availableTaxiRepository.Object, null);
-            var taxi = new Taxi(TaxiSize.Small, 4, true, false, false, false);
-            var customer = new Customer();
             availableTaxiRepository
                     .Setup(x => x.Exists(It.IsAny<string>()))
                     .Returns(false);
@@ -45,14 +53,9 @@ namespace TakeMeThere
         [Test]
         public void ShouldSaveNewBooking()
         {
-            var bookingRepository = new Mock<IBookingRepository>();
-            var availableTaxiRepository = new Mock<IAvailableTaxiRepository>();
-            var api = new Api(availableTaxiRepository.Object, bookingRepository.Object);
-            var taxi = new Taxi(TaxiSize.Small, 4, true, false, false, false);
             availableTaxiRepository
                 .Setup(x => x.Exists(It.IsAny<string>()))
                 .Returns(true);
-            var customer = new Customer();
 
             api.BookTaxi(taxi, customer, new Location(1, 1), new Location(1, 1), 150.3);
 
