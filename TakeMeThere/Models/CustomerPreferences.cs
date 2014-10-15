bd34a4a0c86d0f65b83ce5b9cc4d3b60d456396d
Oscar Moreno <oscar.moreno.martin@gmail.com>
2014-10-15 19:53:28 +0200

    refactor, extract file

---
+++ TakeMeThere/Models/CustomerPreferences.cs
@@ -0,0 +1,12 @@
+﻿namespace TakeMeThere.Models
+{
+    public class CustomerPreferences
+    {
+        public int? TaxiMinimunRating { get; private set; }
+
+        public CustomerPreferences(int? taxiMinimunRating)
+        {
+            TaxiMinimunRating = taxiMinimunRating;
+        }
+    }
+}
