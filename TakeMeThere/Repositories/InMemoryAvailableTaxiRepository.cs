72593c41a0119f4666cd9fbd34ae88e390e0656a
Oscar Moreno <Oscar Moreno@localhost>
2014-10-19 19:58:29 +0200

    Implementing factory methods, inMemoryRepositories, stressTests

---
+++ TakeMeThere/Repositories/InMemoryAvailableTaxiRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using TakeMeThere.Models;
+
+namespace TakeMeThere.Repositories
+{
+    public class InMemoryAvailableTaxiRepository : IAvailableTaxiRepository
+    {
+        public void Save(AvailableTaxi availableTaxi)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Exists(string taxiId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(string taxiId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<AvailableTaxi> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(AvailableTaxi availableTaxi)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TakeMeThere/Repositories/InMemoryBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TakeMeThere.Models;
+
+namespace TakeMeThere.Repositories
+{
+    public class InMemoryBookingRepository : IBookingRepository
+    {
+
+        public void Save(BookingRequest bookingRequest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
