56172d9c9fe186fe45f93325dede22c6c35f1e8f
Oscar Moreno <oscar.moreno.martin@gmail.com>
2014-10-19 19:58:29 +0200

    Implementing factory methods, inMemoryRepositories, stressTests

---
+++ TakeMeThere/Factory.cs
@@ -0,0 +1,28 @@
+﻿using TakeMeThere.Repositories;
+using TakeMeThere.Services;
+
+namespace TakeMeThere
+{
+    public class Factory
+    {
+        public static CommandLineInterface CommandLineInterface()
+        {
+            return new CommandLineInterface(BookingService(), TaxiRegisterService(), TaxiFinder());
+        }
+
+        public static BookingService BookingService()
+        {
+            return new BookingService(new InMemoryAvailableTaxiRepository(), new InMemoryBookingRepository());
+        }
+
+        public static TaxiRegisterService TaxiRegisterService()
+        {
+            return new TaxiRegisterService(new InMemoryAvailableTaxiRepository());
+        }
+
+        public static TaxiFinder TaxiFinder()
+        {
+            return new TaxiFinder(new InMemoryAvailableTaxiRepository());
+        }
+    }
+}
---
+++ TakeMeThere/StressTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace TakeMeThere
+{
+    [TestFixture]
+    public class StressTests
+    {
+        private CommandLineInterface api;
+
+        [SetUp]
+        public void SetUp()
+        {
+            api = Factory.CommandLineInterface();
+        }
+
+    }
+}
