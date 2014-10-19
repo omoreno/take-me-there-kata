6c3f020e64e577115755d2630852446c727b5366
Oscar Moreno <oscar.moreno.martin@gmail.com>
2014-10-19 19:11:04 +0200

    refactor, extract files, extract tests setup

---
+++ TakeMeThere/Repositories/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using TakeMeThere.Models;
+
+namespace TakeMeThere.Repositories
+{
+    public interface ICustomerRepository
+    {
+        void Update(Customer customer);
+    }
+}
---
+++ TakeMeThere/Services/RatingService.cs
@@ -0,0 +1,29 @@
+﻿using TakeMeThere.Models;
+using TakeMeThere.Repositories;
+
+namespace TakeMeThere.Services
+{
+    public class RatingService
+    {
+        private readonly ICustomerRepository customerRepository;
+        private readonly IAvailableTaxiRepository availableTaxiRepository;
+
+        public RatingService(ICustomerRepository customerRepository, IAvailableTaxiRepository availableTaxiRepository)
+        {
+            this.customerRepository = customerRepository;
+            this.availableTaxiRepository = availableTaxiRepository;
+        }
+
+        public void RateCustomer(AvailableTaxi taxi, Customer customer, int rate)
+        {
+            customer.Rate(rate);
+            customerRepository.Update(customer);
+        }
+
+        public void RateTaxi(Customer customer, AvailableTaxi taxi, int rate)
+        {
+            taxi.Rate(rate);
+            availableTaxiRepository.Update(taxi);
+        }
+    }
+}
