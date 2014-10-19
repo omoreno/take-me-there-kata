1c9abfed7d72ec287aa1f3966f6ca2166bac7537
Oscar Moreno <oscar.moreno.martin@gmail.com>
2014-10-19 21:52:06 +0200

    using the main to test the app performance

--- TakeMeThere/Program.cs
+++ TakeMeThere/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Text;
+using TakeMeThere.Models;
 
 namespace TakeMeThere
 {
@@ -7,7 +9,47 @@ namespace TakeMeThere
     {
         static void Main(string[] args)
         {
-            throw new NotImplementedException();
+            var cli = Factory.CommandLineInterface();
+            Console.WriteLine("Creating 1 million Taxis...");
+            var watch = Stopwatch.StartNew();
+            Create1MTaxis(cli);
+            watch.Stop();
+            Console.WriteLine("Time (ms):" + watch.ElapsedMilliseconds);
+            Console.WriteLine("------------------------------------------------");
+
+            Console.WriteLine("Creating 1 million Customers...");
+            watch = Stopwatch.StartNew();
+            var customer = Create1MCustomers(cli);
+            watch.Stop();
+            Console.WriteLine("Time (ms):" + watch.ElapsedMilliseconds);
+            Console.WriteLine("------------------------------------------------");
+
+            Console.WriteLine("Searching...");
+            watch = Stopwatch.StartNew();
+            var taxis = cli.GetTaxis(customer, new Location(1, 1), TaxiSearchFilter.BestRated,
+                         new CustomerNeeds(TaxiSize.Small, 4, true, false, true, true));
+            watch.Stop();
+            Console.WriteLine("Time (ms):" + watch.ElapsedMilliseconds);
+            Console.WriteLine("------------------------------------------------");
+            foreach (var taxi in taxis)
+                Console.WriteLine(taxi.Id);
+        }
+
+        private static void Create1MTaxis(CommandLineInterface cli)
+        {
+            for (var i = 0; i < 1000000; i++)
+                cli.RegisterTaxi(new TaxiFeatures(TaxiSize.Small, 4, true, false, true, true), new Location(100, 100), new TaxiAvailabilityPreferences(TaxiTripLength.Short, null, 10000));
+        }
+
+        private static Customer Create1MCustomers(CommandLineInterface cli)
+        {
+            var customer = new Customer(new CustomerPreferences(null));
+            for (var i = 0; i < 1000000; i++)
+            {
+                customer = new Customer(new CustomerPreferences(null));
+                cli.RegisterCustomer(customer);
+            }
+            return customer;
         }
     }
 }
