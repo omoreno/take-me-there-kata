341c2fffdf35f5384be38c08fe6b24f9d6d1a0ff
Oscar Moreno <Oscar Moreno@localhost>
2014-10-19 21:51:43 +0200

    green ShouldFindTaxisInLessThanASecond

--- TakeMeThere/StressTests.cs
+++ TakeMeThere/StressTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using NUnit.Framework;
+using TakeMeThere.Models;
 
 namespace TakeMeThere
 {
@@ -14,5 +16,37 @@ namespace TakeMeThere
             api = Factory.CommandLineInterface();
         }
 
+        [Test]
+        public void ShouldFindTaxisInLessThanASecond()
+        {
+            Create1MillionTaxis();
+            var customer = Create1MillionCustomers();
+
+            var watch = Stopwatch.StartNew();
+            api.GetTaxis(customer, new Location(1, 1), TaxiSearchFilter.BestRated,
+                             new CustomerNeeds(TaxiSize.Small, 4, true, false, true, true));
+            watch.Stop();
+
+            Assert.LessOrEqual(watch.ElapsedMilliseconds, 1000);
+        }
+
+        private void Create1MillionTaxis()
+        {
+            for (var i = 0; i < 1000000; i++)
+            {
+               api.RegisterTaxi(new TaxiFeatures(TaxiSize.Small, 4, true, false, true, true), new Location(100, 100), new TaxiAvailabilityPreferences(TaxiTripLength.Short, null, 10000));
+            }
+        }
+
+        private Customer Create1MillionCustomers()
+        {
+            var customer = new Customer(new CustomerPreferences(null));
+            for (var i = 0; i < 1000000; i++)
+            {
+                customer = new Customer(new CustomerPreferences(null));
+                api.RegisterCustomer(customer);
+            }
+            return customer;
+        }
     }
 }
