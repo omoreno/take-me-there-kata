4aea689a7d19db6af930e1a53be7f764ffe505e9
Oscar Moreno <oscar.moreno@saludonnet.com>
2014-10-22 10:37:34 +0200

    Implementing repository

--- TakeMeThere/Models/Booking.cs
+++ TakeMeThere/Models/Booking.cs
@@ -6,10 +6,12 @@ namespace TakeMeThere.Models
     {
         public string CustomerId { get; private set; }
         public string TaxiId { get; private set; }
+        public string Reference { get; private set; }
         public DateTime BookingDate { get; private set; }
 
         public Booking(string customerId, string taxiId, DateTime bookingDate)
         {
+            Reference = Guid.NewGuid().ToString();
             CustomerId = customerId;
             TaxiId = taxiId;
             BookingDate = bookingDate;
